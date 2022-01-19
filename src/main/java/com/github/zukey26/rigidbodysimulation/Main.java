197b0f49bb7c78b74967146d21be994f611fc134
zukey26 <atticus.pak@gmail.com>
2022-01-18 18:00:34 -0800

    Update and add a bit of information for future

--- src/main/java/com/github/zukey26/rigidbodysimulation/Main.java
+++ src/main/java/com/github/zukey26/rigidbodysimulation/Main.java
@@ -2,7 +2,9 @@ package com.github.zukey26.rigidbodysimulation;
 
 public class Main {
 
-
+    /**
+    * @param args - optional arguments currently do nothing
+     */
     public static void main(String[] args)
     {
         new Simulation();
--- src/main/java/com/github/zukey26/rigidbodysimulation/Simulation.java
+++ src/main/java/com/github/zukey26/rigidbodysimulation/Simulation.java
@@ -1,4 +1,7 @@
 package com.github.zukey26.rigidbodysimulation;
 
+/**
+ * Main simulation class. Runs off of this.
+ */
 public class Simulation {
 }
