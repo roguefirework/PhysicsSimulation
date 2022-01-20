d700bd277d39f119b718562aad4593dfee048a84
zukey26 <atticus.pak@gmail.com>
2022-01-19 17:04:36 -0800

    Add circle

--- src/main/java/com/github/zukey26/rigidbodysimulation/vector/AABB.java
+++ src/main/java/com/github/zukey26/rigidbodysimulation/vector/AABB.java
@@ -1,4 +1,39 @@
 package com.github.zukey26.rigidbodysimulation.vector;
 
 public class AABB {
+    Vec2 min;
+    Vec2 max;
+
+    public AABB(Vec2 min, Vec2 max) {
+        this.min = min;
+        this.max = max;
+    }
+
+
+    public boolean intersects(AABB other)
+    {
+        if(this.getMax().getX() < other.getMin().getX() || this.min.getX() > other.getMax().getX())
+        {
+            return false;
+        }
+        return !(this.getMax().getY() < other.getMin().getY()) && !(this.min.getY() > other.getMax().getY());
+    }
+
+
+
+    public Vec2 getMin() {
+        return min;
+    }
+
+    public void setMin(Vec2 min) {
+        this.min = min;
+    }
+
+    public Vec2 getMax() {
+        return max;
+    }
+
+    public void setMax(Vec2 max) {
+        this.max = max;
+    }
 }
---
+++ src/main/java/com/github/zukey26/rigidbodysimulation/vector/Circle.java
@@ -0,0 +1,35 @@
+package com.github.zukey26.rigidbodysimulation.vector;
+
+public class Circle {
+    float radius;
+    Vec2 position;
+
+    public Circle(float radius, Vec2 position) {
+        this.radius = radius;
+        this.position = position;
+    }
+
+
+    public boolean collidesOther(Circle other)
+    {
+        float r = this.radius + other.getRadius();
+        r *= r;
+        return r < ((position.getX() + other.getPosition().getX()) + (position.getY() + other.getPosition().getY()));
+    }
+
+    public float getRadius() {
+        return radius;
+    }
+
+    public void setRadius(float radius) {
+        this.radius = radius;
+    }
+
+    public Vec2 getPosition() {
+        return position;
+    }
+
+    public void setPosition(Vec2 position) {
+        this.position = position;
+    }
+}
